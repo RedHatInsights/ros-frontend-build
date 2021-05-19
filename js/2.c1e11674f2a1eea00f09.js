8faf0e80d17c7cc95bb8bddd5fb9c32bf504e99f
Travis CI User <travis@example.org>
2021-05-19 07:29:58 +0000

    RHIROS_146 - remove wrapper around route & clean up code (#39)

--- js/2.c1e11674f2a1eea00f09.js
+++ js/2.c1e11674f2a1eea00f09.js
@@ -1,2 +1,2 @@
 (self.webpackChunkros_frontend=self.webpackChunkros_frontend||[]).push([[2],{93002:(e,n,t)=>{"use strict";t.r(n);var r=t(1427),o=t.n(r),a=t(88696),l=t.n(a),c=t(16530),s=t(80962),u=t(55206),m=t(21459),d=t(47109);const f=function(){return o().createElement(s.Provider,{store:(0,u.S1)().getStore()},o().createElement(c.BrowserRouter,{basename:(0,d.Z)(window.location.pathname)},o().createElement(m.Z,null)))};l().render(o().createElement(f,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=2.ad10f0647a01028afd4b.js.map
+//# sourceMappingURL=2.c1e11674f2a1eea00f09.js.map
