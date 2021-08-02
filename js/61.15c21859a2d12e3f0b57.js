817498039eda828779f4e96df17cefb50669cac1
Travis CI User <travis@example.org>
2021-08-02 08:42:27 +0000

    update @redhat-cloud-services/* packages (#49)
    * update @redhat-cloud-services/* packages
    
    * updated FE components utils to 3.2.3, installed sass
    
    and removed deprecated node-sass

---
+++ js/61.15c21859a2d12e3f0b57.js
@@ -0,0 +1,2 @@
+(self.webpackChunkros_frontend=self.webpackChunkros_frontend||[]).push([[61],{58061:(s,e,n)=>{"use strict";n.d(e,{RC:()=>r,M4:()=>t,Up:()=>a,RM:()=>o,nV:()=>p,xP:()=>f});var r="/api/ros/v0",t="/systems",a="/rating",o=-1,p=0,f=1}}]);
+//# sourceMappingURL=../sourcemaps/61.09fee824c9d6f2c03e4d.js.map
