ab2ceda779add1d7320c3c6e5b1a9cddf8aa7a09
Travis CI User <travis@example.org>
2021-08-05 09:02:46 +0000

    Merge pull request #51 from RedHatInsights/stable
    updating prod beta

---
+++ js/2.4880a210bef2e777fb2c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkros_frontend=self.webpackChunkros_frontend||[]).push([[2],{93002:(e,n,t)=>{"use strict";t.r(n);var r=t(1427),o=t.n(r),a=t(88696),l=t.n(a),c=t(16530),s=t(80962),u=t(55206),m=t(46755),d=t(47109);const f=function(){return o().createElement(s.Provider,{store:(0,u.S1)().getStore()},o().createElement(c.BrowserRouter,{basename:(0,d.Z)(window.location.pathname)},o().createElement(m.Z,null)))};l().render(o().createElement(f,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=2.4880a210bef2e777fb2c.js.map
