749e8b429ad31a3eb073e1c89bae3f2b527bc316
Travis CI User <travis@example.org>
2021-11-23 12:33:26 +0000

    RHIROS-388 - fixed store already initialized error (#71)
    * RHIROS-388 - fixed store already initialized error
    
    * fixed lint errors
    
    * updated code for using redux-logger

--- js/412.6ccd8f8e8d621405df49.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkros_frontend=self.webpackChunkros_frontend||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>f});var r=n(1427),o=n.n(r),a=n(16530),s=n(37703),c=n(30319),l=n(99621),u=n(35240);const f=function(){return o().createElement(s.zt,{store:(0,c.S1)().getStore()},o().createElement(a.BrowserRouter,{basename:(0,u.eb)(window.location.pathname,2)},o().createElement(l.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.c3465dbd3d02a8fe2a76eb3a0ff637f7.js.map
